ada56c34e6adc391cb87d3f927752a8c3beff878
rpachotome <rpachotome@eniac32.danielcastelao.org>
2015-01-15 09:05:37 +0100

    Adestrador comentao

--- Boletin15/src/boletin15/Adestrador.java
+++ Boletin15/src/boletin15/Adestrador.java
@@ -1,32 +1,52 @@
 package boletin15;
 
 import javax.swing.JOptionPane;
-
+/**
+ * 
+ * @author rpachotome
+ */
 public class Adestrador extends Equipo {
 
     private String idFederacion;
-
+/**
+ * Constructor vacío
+ */
     public Adestrador() {
 
     }
-
+/**
+ * Contstructor con parámetros
+ * @param id
+ * @param nome
+ * @param apelidos
+ * @param idade
+ * @param idFederacion 
+ */
     public Adestrador(int id, String nome, String apelidos, int idade, String idFederacion) {
 
     }
-
+/**
+ * Metodo vacío que no devuelve nada
+ */
     public void dirixirPartido() {
 
     }
-
+/**
+ * Metodo vacío que no devuelve nada
+ */
     public void dirixirAdestramento() {
 
     }
-
+/**
+*Se sobreescribe el metodo de la superclase con un mensaje diferente que mostrar por pantalla
+*/
     @Override
     public void viaxar() {
         JOptionPane.showMessageDialog(null, "Viaxan os adestradores");
     }
-
+/**
+*Se sobreescribe el metodo de la superclase con toString que une la información
+*/
     @Override
     public String toString() {
         return super.toString() + "\nID da Federación:  " + idFederacion;
